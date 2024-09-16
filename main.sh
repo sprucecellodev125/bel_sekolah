e2ef470c70e73be68b20df79394e923a035495fa
sprucecellodev125 <91181632+sprucecellodev125@users.noreply.github.com>
2024-09-16 14:32:13 +0700

    Fixed NVM path sourcing

--- main.sh
+++ main.sh
@@ -26,12 +26,16 @@ start() {
         if ! command -v node &> /dev/null; then
             if command -v wget &> /dev/null; then
                 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-                source ~/.bashrc
+                export NVM_DIR="$HOME/.nvm"
+                [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+                [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
                 nvm install node
                 npm i -g pm2
             elif command -v curl &> /dev/null; then
                 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-                source ~/.bashrc
+                export NVM_DIR="$HOME/.nvm"
+                [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+                [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
                 nvm install node
                 npm i -g pm2
             else
